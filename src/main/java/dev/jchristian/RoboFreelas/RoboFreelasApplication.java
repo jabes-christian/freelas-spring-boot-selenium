70d6ee4d5eb8652599c6a8f3ffcc190bd3d72384
Jabes Christian <jabescristian08@gmail.com>
2026-03-28 17:04:15 -0300

    primeiro commit do projeto do robo de monitoramento de freelas

---
+++ src/main/java/dev/jchristian/RoboFreelas/RoboFreelasApplication.java
@@ -0,0 +1,13 @@
+package dev.jchristian.RoboFreelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboFreelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboFreelasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jchristian/RoboFreelas/RoboFreelasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jchristian.RoboFreelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboFreelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
